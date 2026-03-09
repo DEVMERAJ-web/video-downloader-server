c61516a3e17b6b069dd2fb387b6b2e4a8d4d64a2
DEVMERAJ-web <saifmeraj78@gmail.com>
2026-03-09 10:35:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const { exec } = require("child_process");
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 app.post("/download", (req, res) => {
   const { url } = req.body;
 
-  exec(`yt-dlp -g ${url}`, (err, stdout) => {
-    if (err) return res.json({ error: "download failed" });
+  if (!url) {
+    return res.json({ error: "No URL provided" });
+  }
+
+  exec(`yt-dlp -g "${url}"`, (err, stdout, stderr) => {
+    if (err) {
+      console.log(stderr);
+      return res.json({ error: "download failed" });
+    }
 
     res.json({ link: stdout.trim() });
   });
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(3000, () => {
+  console.log("Downloader server running");
+});
