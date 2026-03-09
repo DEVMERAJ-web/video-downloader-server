a7d8b24e854d8ceb1b747121c8826719d6aed9d2
DEVMERAJ-web <saifmeraj78@gmail.com>
2026-03-09 10:55:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const { exec } = require("child_process");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.post("/download", (req, res) => {
   const { url } = req.body;
 
@@ -5,7 +14,7 @@ app.post("/download", (req, res) => {
     return res.json({ error: "No URL provided" });
   }
 
-  exec(`yt-dlp -f best -g "${url}"`, (err, stdout, stderr) => {
+  exec(`yt-dlp -f "bestvideo+bestaudio/best" -g "${url}"`, (err, stdout, stderr) => {
     if (err) {
       console.log(stderr);
       return res.json({ error: "download failed" });
@@ -13,4 +22,8 @@ app.post("/download", (req, res) => {
 
     res.json({ link: stdout.trim() });
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
